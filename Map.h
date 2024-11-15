367b3c7f70cfad1e749db6c040f8c4d9c8fc4ec2
hyunholee <dionme2622@gmail.com>
2024-11-15 18:50:03 +0900

    CVillage, CPirate 맵 클래스 생성

--- Map.h
+++ Map.h
@@ -5,17 +5,31 @@ public:
 	CMap() {};
 	~CMap() {};
 
+	virtual void Initialize();
 private:
 
 };
 
 class CVillage : public CMap
 {
+public :
+	CVillage() {};
+	~CVillage() {};
+
+	virtual void Initialize();
+private:
 
 
 };
 
 class CPirate : public CMap
 {
+public:
+	CPirate() {};
+	~CPirate() {};
+
+	virtual void Initialize();
+private:
+
 
 };
